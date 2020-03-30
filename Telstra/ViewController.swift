fb38c002761450a776c60268995d555d10fcd797
Alvis <alvis.mach@quirqy.com>
2020-03-30 20:35:18 +1100

    Initial Commit

---
+++ Telstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telstra
+//
+//  Created by suitecontrol on 30/3/20.
+//  Copyright © 2020 suitecontrol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
